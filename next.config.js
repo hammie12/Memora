d22eed6f57a3ea991dd3174954ee9008c5b4544d
Hamed Bakayoko <90972356+hammie12@users.noreply.github.com>
2025-05-11 23:38:17 +0100

    newest fix

--- next.config.js
+++ next.config.js
@@ -10,13 +10,6 @@ const nextConfig = {
       },
     ],
   },
-  eslint: {
-    // We want to see ESLint errors during development but ignore the specific
-    // 'src/types/supabase.ts' file during the build process if it causes issues.
-    // Setting ignoreDuringBuilds: false makes sure ESLint runs during build.
-    // ignorePatterns will exclude the specified file from linting.
-    ignoreDuringBuilds: false,
-  },
   // Other Next.js config options can go here
 };
 
